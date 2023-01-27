3729aa21962fcec3ebd278d5fac522766ebaf100
Mahãyana Vitória <mahayana@vetorlog.com>
2023-01-27 10:59:49 -0300

    commit 2

--- CHANGE_STYLE/js.js
+++
@@ -1,6 +0,0 @@
-function js_style() {
-  document.getElementById('text').style.color="blue"
-  document.getElementById('text').style.fontFamily ="arial"
-  document.getElementById('text').style.fontSize ="18px"
-  console.log("ok")
-}
